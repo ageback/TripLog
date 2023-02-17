2cd3cf33025d0165d639b30f669f8fe89dbb18e7
Michael Lu <yeskky@gmail.com>
2023-02-17 14:17:32 +0800

    MVVM

--- TripLog/TripLog/Views/NewEntryPage.xaml.cs
+++ TripLog/TripLog/Views/NewEntryPage.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TripLog.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +13,31 @@ namespace TripLog.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class NewEntryPage : ContentPage
     {
+        NewEntryViewModel ViewModel => BindingContext as NewEntryViewModel;
         public NewEntryPage()
         {
             InitializeComponent();
+
+            BindingContextChanged += Page_BindingContextChanged;
+            BindingContext = new NewEntryViewModel();
+        }
+
+        void Page_BindingContextChanged(object sender, EventArgs e) => ViewModel.ErrorsChanged += ViewModel_ErrorsChanged;
+
+        void ViewModel_ErrorsChanged(object sender, DataErrorsChangedEventArgs e)
+        {
+            var propHasErrors = (ViewModel.GetErrors(e.PropertyName) as List<string>)?.Any() == true;
+            switch (e.PropertyName)
+            {
+                case nameof(ViewModel.Title):
+                    title.LabelColor = propHasErrors ? Color.Red : Color.Black;
+                    break;
+                case nameof(ViewModel.Rating):
+                    rating.LabelColor = propHasErrors ? Color.Red : Color.Black;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
