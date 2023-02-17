195d48b0df2f370b73b7f3d6b266ccdfb138ea13
Michael Lu <yeskky@gmail.com>
2023-02-17 16:51:30 +0800

    navigation

---
+++ TripLog/TripLog/Services/INavService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TripLog.ViewModels;
+
+namespace TripLog.Services
+{
+    public interface INavService
+    {
+        bool CanGoBack { get; }
+        Task GoBack();
+        Task NavigateTo<TVM>() where TVM : BaseViewModel;
+        Task NavigateTo<TVM, TParameter>(TParameter parameter) where TVM : BaseViewModel;
+        void RemoveLastView();
+        void ClearBackStack();
+        void NavigateToUri(Uri uri);
+
+        event PropertyChangedEventHandler CanGoBackChanged;
+    }
+}
